38c339016fada60ea2e31b55379aa31cf0573b69
Victoryland <faisal.ahmed0001@gmail.com>
2014-04-03 17:32:54 +0600

    Initial Structure

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=7"/>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <title>Coursefinderdemo</title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/jquery.wysiwyg.css"/>
+    <link rel="stylesheet" type="text/css" href="css/facebox.css"/>
+    <link rel="stylesheet" type="text/css" href="css/visualize.css"/>
+    <link rel="stylesheet" type="text/css" href="css/date_input.css"/>
+
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" type="text/css" href="css/ie.css"/><![endif]-->
+
+</head>
+
+<body>
+
+<div id="hld">
+    <div class="wrapper">        <!-- wrapper begins -->
+        <?php include_once 'courseImporter.php' ?>
+    </div>
+    <!-- wrapper ends -->
+</div>
+<!-- #hld ends -->
+
+<!--[if IE]>
+<script type="text/javascript" src="js/excanvas.js"></script><![endif]-->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.img.preload.js"></script>
+<script type="text/javascript" src="js/jquery.filestyle.mini.js"></script>
+<script type="text/javascript" src="js/jquery.wysiwyg.js"></script>
+<script type="text/javascript" src="js/jquery.date_input.pack.js"></script>
+<script type="text/javascript" src="js/facebox.js"></script>
+<script type="text/javascript" src="js/jquery.visualize.js"></script>
+<script type="text/javascript" src="js/jquery.visualize.tooltip.js"></script>
+<script type="text/javascript" src="js/jquery.select_skin.js"></script>
+<script type="text/javascript" src="js/jquery.tablesorter.min.js"></script>
+<script type="text/javascript" src="js/ajaxupload.js"></script>
+<script type="text/javascript" src="js/jquery.pngfix.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+</html>
